66d3bc7ae6acbbb8ae4b33d659895c51b27add3f
Rebeka Csóh <rebeka.csoh@gmail.com>
2025-07-28 18:04:56 +0200

    program első rész

--- Encryption/Program.cs
+++ Encryption/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Encryption
+﻿using Encryption.Models;
+
+namespace Encryption
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Secrecy secrecy = new Secrecy();
+
+            Console.Write("Üzenet: ");
+            string message = Console.ReadLine();
+            int length = message.Length;
+
+            string key = secrecy.keyGenerator(length);
+            Console.WriteLine($"Kulcs: {key}");
+
+            string encrypted = secrecy.Encrypting(message, key);
+            Console.WriteLine($"Titkosított üzenet: {encrypted}");
+
+            string decodedMessage = secrecy.Decoding(key, encrypted);
+            Console.WriteLine($"Eredeti üzenet: {decodedMessage}");
         }
     }
 }
