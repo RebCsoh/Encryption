6d8130a402d91a2fd49e436c84295ff70e43b767
Rebeka Csóh <rebeka.csoh@gmail.com>
2025-07-28 16:50:34 +0200

    kulcs generálás

---
+++ Encryption/Secrery.cs
@@ -0,0 +1,22 @@
+﻿namespace Encryption
+{
+    public class Secrery
+    {
+        // kulcs generálás
+        string abc = "abcdefghijklmnopqrstuvwxyz ";
+        public string keyGenerator()
+        {
+            string GeneratedKey = string.Empty;
+            Random random = new Random();
+            int lenght = random.Next(1,100);
+
+            for (int i = 0; i < lenght; i++)
+            {
+                int number = random.Next(0,27);
+                int lettre = abc[number];
+                GeneratedKey += lettre;
+            }
+            return GeneratedKey;
+        }
+    }
+}
