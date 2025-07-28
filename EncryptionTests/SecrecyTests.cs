4fe108c3c227830090830e7c8c41a8f4601cc66d
Rebeka Csóh <rebeka.csoh@gmail.com>
2025-07-28 18:29:05 +0200

    tesztelés1

---
+++ EncryptionTests/SecrecyTests.cs
@@ -0,0 +1,25 @@
+﻿using Encryption.Models;
+using NUnit.Framework;
+
+namespace EncryptionTests
+{
+    [TestFixture]
+    public class SecrecyTests
+    {
+        [TestCase("alma","aaaa","alma")]
+        [TestCase("abcd","bbbb","bcde")]
+        [TestCase("helloworld", "abcdefgijkl", "hfnosauzun")]
+
+        public void EncryptingTest(string message, string key, string expectedResult)
+        {
+            //arrange
+            Secrecy secrecy = new Secrecy();
+
+            //act
+            string result = secrecy.Encrypting(message, key);
+
+            //assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
